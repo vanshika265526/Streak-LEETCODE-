b6067018007be80e7efc53b8d15cb3e6cf99d09f
Vanshika <166399561+vanshika265526@users.noreply.github.com>
2026-01-25 20:18:52 +0530

    Create Minimum Difference Between Highest and Lowest of K Scores.cpp

---
+++ Minimum Difference Between Highest and Lowest of K Scores.cpp
@@ -0,0 +1,18 @@
+/*
+You are given a 0-indexed integer array nums, where nums[i] represents the score of the ith student. You are also given an integer k.
+
+Pick the scores of any k students from the array so that the difference between the highest and the lowest of the k scores is minimized.
+
+Return the minimum possible difference.
+*/
+class Solution {
+public:
+    int minimumDifference(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int ans=INT_MAX;
+        for(int i=0;i+k-1<nums.size();i++){
+            ans=min(ans,nums[i+k-1]-nums[i]);
+        }
+        return ans;
+    }
+};
