c63ab7f60da3cdc2c6fd1b9d88a9871c51605a54
Vanshika <166399561+vanshika265526@users.noreply.github.com>
2026-02-01 17:58:18 +0530

    Update Divide an Array Into Subarrays With Minimum Cost I.cpp

--- Divide an Array Into Subarrays With Minimum Cost I.cpp
+++ Divide an Array Into Subarrays With Minimum Cost I.cpp
@@ -20,3 +20,24 @@ public:
         return count;
     }
 };
+
+
+//METHOD 2
+class Solution {
+public:
+    int minimumCost(vector<int>& nums) {
+        int first = INT_MAX, second = INT_MAX;
+
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] < first) {
+                second = first;
+                first = nums[i];
+            } else if (nums[i] < second) {
+                second = nums[i];
+            }
+        }
+
+        return nums[0] + first + second;
+    }
+};
+
