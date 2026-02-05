0f87422b3e56f249f379f525f683c2384be794b7
Vanshika <166399561+vanshika265526@users.noreply.github.com>
2026-02-05 21:03:00 +0530

    Create Transformed Array.cpp

---
+++ Transformed Array.cpp
@@ -0,0 +1,27 @@
+/*
+You are given an integer array nums that represents a circular array. Your task is to create a new array result of the same size, following these rules:
+
+For each index i (where 0 <= i < nums.length), perform the following independent actions:
+If nums[i] > 0: Start at index i and move nums[i] steps to the right in the circular array. Set result[i] to the value of the index where you land.
+If nums[i] < 0: Start at index i and move abs(nums[i]) steps to the left in the circular array. Set result[i] to the value of the index where you land.
+If nums[i] == 0: Set result[i] to nums[i].
+Return the new array result.
+  */
+class Solution {
+public:
+    vector<int> constructTransformedArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> result(n);
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) {
+                result[i] = 0;
+            } else {
+                int move = nums[i];
+                int newIndex = (i + move % n + n) % n;
+                result[i] = nums[newIndex];
+            }
+        }
+        return result;
+    }
+};
