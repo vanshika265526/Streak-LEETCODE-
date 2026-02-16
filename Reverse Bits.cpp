8191764fb9bd75952227dc9818cab02f4d214a53
Vanshika <166399561+vanshika265526@users.noreply.github.com>
2026-02-16 22:31:58 +0530

    Create Reverse Bits.cpp

---
+++ Reverse Bits.cpp
@@ -0,0 +1,32 @@
+/*
+Reverse bits of a given 32 bits signed integer.
+
+ 
+
+Example 1:
+
+Input: n = 43261596
+
+Output: 964176192
+
+Explanation:
+
+Integer	Binary
+43261596	00000010100101000001111010011100
+964176192	00111001011110000010100101000000
+
+  */
+
+class Solution {
+public:
+    int reverseBits(int n) {
+        unsigned int result = 0;
+
+        for (int i = 0; i < 32; i++) {
+            result = (result << 1) | (n & 1); // extract last bit
+            n >>= 1; // shift right
+        }
+
+        return result;
+    }
+};
