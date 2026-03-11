75083dbf73de51dde4ee0c3de7e542c9ede21aa4
Vanshika <166399561+vanshika265526@users.noreply.github.com>
2026-03-11 22:18:31 +0530

    Create Complement of Base 10 Integer.cpp

---
+++ Complement of Base 10 Integer.cpp
@@ -0,0 +1,29 @@
+/*
+The complement of an integer is the integer you get when you flip all the 0's to 1's and all the 1's to 0's in its binary representation.
+
+For example, The integer 5 is "101" in binary and its complement is "010" which is the integer 2.
+Given an integer n, return its complement.
+
+ 
+
+Example 1:
+
+Input: n = 5
+Output: 2
+Explanation: 5 is "101" in binary, with complement "010" in binary, which is 2 in base-10.
+  */
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        
+        if(n==0) return 1;
+        int mask=0;
+        int temp=n;
+        while(temp>0){
+            mask=(mask<<1)|1;
+            temp>>=1;
+
+        }
+        return n^mask;
+    }
+};
